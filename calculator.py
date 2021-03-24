afa421835ed75987a0b3b950cc0c53a12a00e41d
dahee <sayurrdahh@gmail.com>
2021-03-24 14:29:06 +0900

    Revert "Revert "Add square function""
    This reverts commit ae03c9caf5fc7c25d09f3c4cef3ff84a2ff85230.

--- calculator.py
+++ calculator.py
@@ -6,11 +6,7 @@ def substract(a, b):
 	return a-b
 
 def divide_free(a,b):
-<<<<<<< HEAD
 	return a/b
 
 def square(a):
 	return a*a
-=======
-	return a/b
->>>>>>> parent of f3e0903... Add square function
